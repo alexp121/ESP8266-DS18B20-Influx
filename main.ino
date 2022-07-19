abcdddabb447a48a262d1fd0af2393a347b75f4c
alexp121 <34182834+alexp121@users.noreply.github.com>
2022-07-19 15:15:47 +1000

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <InfluxDb.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 4
+#define INFLUXDB_HOST "192.168.0.146"
+#define WIFI_SSID "SSID"
+#define WIFI_PASS "PASSWORD"
+ESP8266WiFiMulti WiFiMulti;
+Influxdb influx(INFLUXDB_HOST);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WIFI");
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  influx.setDb("hvac");
+  Serial.println("Setup Complete.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  InfluxData row("room1");
+  row.addValue("temperature", random(1, 20));
+  influx.write(row);
+  delay(20000);
+  long t = millis();
+}
